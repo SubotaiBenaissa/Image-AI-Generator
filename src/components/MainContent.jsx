9cd99c156611138aee160a2bbc81f05a813f85c7
Alangobe <alangobe01@gmail.com>
2023-08-07 21:11:45 -0500

    Intalado inter y maquetado el header de la página

--- src/components/MainContent.jsx
+++
@@ -1,7 +0,0 @@
-
-
-export const MainContent = () => {
-  return (
-    <div>MainContent</div>
-  )
-}
---
+++ src/pages/MainContent.jsx
@@ -0,0 +1,11 @@
+import { Header } from "../components/Header"
+
+export const MainContent = () => {
+
+    return (
+      <div>
+        <Header />
+      </div>
+    )
+
+}
