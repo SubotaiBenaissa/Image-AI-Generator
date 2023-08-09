4e2e83848a6eb0aeb53991867ddb03d556e8eff6
Alangobe <alangobe01@gmail.com>
2023-08-08 22:12:03 -0500

    wtf xd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export const Header = () => {
     return (
         <div className='header-layout'>
             <h1 className="title">ImageBIT <span className='AI'>AI</span></h1>
-            <p className='header-text'>Genera imagenes a tu gusto usando inteligencia artificial</p>
+            <p className='header-text'>Genera imágenes a tu gusto usando inteligencia artificial</p>
         </div>
     )
 
